8ab9df44c63ac6f94492eddc3aae717945f4aa51
amanjain39400 <39668341+amanjain39400@users.noreply.github.com>
2020-03-06 15:59:15 +0530

    ghh

--- Winwheel.js
+++ Winwheel.js
@@ -87,7 +87,7 @@ function Winwheel(options, drawWheel)
     // ------------------------------------------
     // If the id of the canvas is set, try to get the canvas as we need it for drawing.
     if (this.canvasId) {
-        this.canvas = document.getElementById(this.canvasId);
+        this.canvas=document.getElementById(this.canvasId);
 
         if (this.canvas) {
             // If the centerX and centerY have not been specified in the options then default to center of the canvas
